d656e485c0302cfdfb91d8e0e7d13e0f1a0a9644
KNULL-git <72454460+KNULL-git@users.noreply.github.com>
2023-08-10 19:01:19 +0530

    Update PCAP-Reader.py

--- PCAP-Reader.py
+++ PCAP-Reader.py
@@ -1,41 +1,104 @@
-import dpkt
 import socket
-from colorama import Fore
+import mysql.connector
+from scapy.all import *
 
-counter=0
-ipcounter=0
-tcpcounter=0
-udpcounter=0
+# MySQL server credentials
+mysql_host = "mysql_host_name"
+mysql_user = "mysql_user_name"
+mysql_password = "mysql_password"
+mysql_database = "mysql_database"
 
-for ts,pkt in dpkt.pcap.Reader(open("")):
-    counter+=1
-    eth=dpkt.ethernet.Ethernet(pkt)
-    if eth.type!=dpkt.ethernet.ETH_TYPE_IP:
-        continue
+# Establish MySQL connection
+try:
+    mysql_conn = mysql.connector.connect(
+        host=mysql_host,
+        user=mysql_user,
+        password=mysql_password,
+        database=mysql_database
+    )
+    mysql_cursor = mysql_conn.cursor()
+    print("Connected to MySQL server")
+except mysql.connector.Error as err:
+    print("Error connecting to MySQL server:", err)
+    exit()
 
-    ip=eth.data
-    ipcounter+=1
-    print(Fore.RESET + 'Source: ' + socket.inet_ntoa(ip.src)+'Destination: '+socket.inet_ntoa(ip.dst))
-    if ip.p==dpkt.ip.IP_PROTO_TCP:
-        tcpcounter+=1
+# Create tables for each protocol
+protocols = ['tcp', 'udp', 'ipv4', 'ipv6', 'icmp', 'arp', 'dns', 'dhcp']
 
-    elif ip.p==dpkt.ip.IP_PROTO_UDP:
-        udpcounter+=1
+for protocol in protocols:
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {protocol}_packets (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        source_ip VARCHAR(45),
+        destination_ip VARCHAR(45),
+        source_port INT,
+        destination_port INT,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """
+    mysql_cursor.execute(create_table_query)
+    mysql_conn.commit()
+    print(f"Table {protocol}_packets created")
 
-def printPCAP(pcap):
-    for (ts,pkt) in dpkt.pcap.Reader(open("",'rb')):
-        try:
-            eth=dpkt.ethernet.Ethernet(pkt)
-            ip= eth.data
-            src= socket.inet_ntoa(ip.src)
-            dst= socket.inet_ntoa(ip.dst)
-            print('Source: '+ src + 'Destination: '+ dst)
-        except:
-            pass
+# Read pcap file
+packets = rdpcap("pcap_file_with_the_complete_path.pcap")
 
+# Process and store packets
+for pkt in packets:
+    src_ip = None
+    dst_ip = None
+    src_port = None
+    dst_port = None
 
-print(Fore.RED + "Total number of packets in the pcap file:", counter)
-print(Fore.YELLOW + "Total number of ip packets in the pcap file:", ipcounter)
-print(Fore.MAGENTA + "Total number of tcp packets in the pcap file:", tcpcounter)
-print(Fore.GREEN + "Total number of udp packets in the pcap file:", udpcounter)
-printPCAP("")
+    if IP in pkt:
+        src_ip = pkt[IP].src
+        dst_ip = pkt[IP].dst
+
+    protocol = None
+    if TCP in pkt:
+        protocol = 'tcp'
+        src_port = pkt[TCP].sport
+        dst_port = pkt[TCP].dport
+        print(f"TCP Packet - Source: {src_ip}:{src_port}, Destination: {dst_ip}:{dst_port}")
+    elif UDP in pkt:
+        protocol = 'udp'
+        src_port = pkt[UDP].sport
+        dst_port = pkt[UDP].dport
+        print(f"UDP Packet - Source: {src_ip}:{src_port}, Destination: {dst_ip}:{dst_port}")
+    elif IP in pkt:
+        protocol = 'ipv4'
+        print(f"IPv4 Packet - Source: {src_ip}, Destination: {dst_ip}")
+    elif IPv6 in pkt:
+        protocol = 'ipv6'
+        print(f"IPv6 Packet - Source: {src_ip}, Destination: {dst_ip}")
+    elif ICMP in pkt:
+        protocol = 'icmp'
+        print(f"ICMP Packet - Source: {src_ip}, Destination: {dst_ip}")
+    elif ARP in pkt:
+        protocol = 'arp'
+        src_ip = pkt[ARP].psrc
+        dst_ip = pkt[ARP].pdst
+        print(f"ARP Packet - Source IP: {src_ip}, Destination IP: {dst_ip}")
+    elif DNS in pkt:
+        protocol = 'dns'
+        print(f"DNS Packet - Source IP: {src_ip}, Destination IP: {dst_ip}")
+    elif DHCP in pkt:
+        protocol = 'dhcp'
+        print(f"DHCP Packet - Source IP: {src_ip}, Destination IP: {dst_ip}")
+    else:
+        protocol = 'unknown'
+        print("Unknown Packet")
+
+    # Insert packet information into appropriate protocol table
+    if protocol != 'unknown':
+        insert_query = f"""
+        INSERT INTO {protocol}_packets (source_ip, destination_ip, source_port, destination_port)
+        VALUES (%s, %s, %s, %s)
+        """
+        values = (src_ip, dst_ip, src_port, dst_port)
+        mysql_cursor.execute(insert_query, values)
+        mysql_conn.commit()
+
+# Close MySQL connection
+mysql_cursor.close()
+mysql_conn.close()
